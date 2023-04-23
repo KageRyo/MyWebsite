591ed57be413aa5a7ae7b640325411746158202d
KageRyo <k6.developer@gmail.com>
2023-04-24 00:05:05 +0800

    Add comment for JavaScript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+// 點擊未完成功能時的事件
 document.addEventListener("DOMContentLoaded", function() {
     const unfinishedFeatures = document.querySelectorAll(".unfinished-feature");
     const modal = document.querySelector(".ts-modal");
     const closeButton = document.getElementById("close-modal-button");
 
+    // 未完成功能的點擊事件
     unfinishedFeatures.forEach(feature => {
         feature.addEventListener("click", function(event) {
             event.preventDefault();
@@ -11,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    // 關閉未完成功能的點擊事件
     closeButton.addEventListener("click", function(event) {
         event.preventDefault();
         modal.classList.remove("is-visible");
