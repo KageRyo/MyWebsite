4e60e8619a31dce13043acd17a87d3b4ef98b0ff
KageRyo <k6.developer@gmail.com>
2023-04-26 01:15:18 +0800

    Arrow functions for feature click handlers.
    Refactor event listeners to use arrow functions in unfinished feature click handler.
    
    Thank @mmm25002500 for the suggestion.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
 // 點擊未完成功能時的事件
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     const unfinishedFeatures = document.querySelectorAll(".unfinished-feature");
     const modal = document.querySelector(".ts-modal");
     const closeButton = document.getElementById("close-modal-button");
-
+  
     // 未完成功能的點擊事件
     unfinishedFeatures.forEach(feature => {
-        feature.addEventListener("click", function(event) {
-            event.preventDefault();
-            modal.classList.remove("is-hidden");
-            modal.classList.add("is-visible");
-        });
-    });
-
-    // 關閉未完成功能的點擊事件
-    closeButton.addEventListener("click", function(event) {
+      feature.addEventListener("click", event => {
         event.preventDefault();
-        modal.classList.remove("is-visible");
-        modal.classList.add("is-hidden");
+        modal.classList.remove("is-hidden");
+        modal.classList.add("is-visible");
+      });
     });
-});
+  
+    // 關閉未完成功能的點擊事件
+    closeButton.addEventListener("click", event => {
+      event.preventDefault();
+      modal.classList.remove("is-visible");
+      modal.classList.add("is-hidden");
+    });
+  });
+  
