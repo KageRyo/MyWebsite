cda6c9afbaf9da93bf8eebde9e7fae1c91c368c7
Ryo <k6.developer@gmail.com>
2023-04-25 14:14:38 +0800

    Add warning message to warning.js file.

---
+++ assets/js/warning.js
@@ -0,0 +1,6 @@
+console.warn("警告：您正在使用開發者工具。");
+console.warn("這是很危險的行為，請不要複製貼上任何東西到這裡。");
+console.warn("如果有人要求您這麼做，那麼他們正在試圖騙取您的資料。");
+console.warn("如果您不知道這是什麼，那麼您不應該在這裡。");
+console.warn("如果您知道這是什麼，那麼您應該知道這是危險的。");
+console.warn("有任何問題歡迎至此專案 GitHub 發佈 Issues。");
