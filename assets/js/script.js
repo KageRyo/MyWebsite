d283e872a9b4e7dfba6dedb37a62f616c454a3a0
Ryo <k6.developer@gmail.com>
2023-04-28 11:23:39 +0800

    Fixed console error messages.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // 點擊未完成功能時的事件
+/*
 document.addEventListener("DOMContentLoaded", () => {
   const unfinishedFeatures = document.querySelectorAll(".unfinished-feature");
   const modal = document.querySelector(".ts-modal");
@@ -20,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
     modal.classList.add("is-hidden");
   });
 });
+*/
 
 // 自 GitHub 抓取專案資料的程式
 document.addEventListener('DOMContentLoaded', function () {
@@ -30,22 +32,25 @@ document.addEventListener('DOMContentLoaded', function () {
     .then((response) => response.json())
     .then((data) => {
       const projectsContainer = document.querySelector('.projects');
-      data.forEach((repo) => {
-        const repoName = repo.name;
-        const repoUrl = repo.html_url;
-        const repoDescription = repo.description || '無描述';
-        const projectCard = `
-                  <div class="ts-card">
-                      <div class="content">
-                          <div class="header">
-                              <a href="${repoUrl}" target="_blank">${repoName}</a>
-                          </div>
-                          <div class="description">
-                              ${repoDescription}
-                          </div>
-                      </div>
-                  </div>`;
-        projectsContainer.insertAdjacentHTML('beforeend', projectCard);
-      });
+
+      if (projectsContainer) {
+        data.forEach((repo) => {
+          const repoName = repo.name;
+          const repoUrl = repo.html_url;
+          const repoDescription = repo.description || '無描述';
+          const projectCard = `
+            <div class="ts-card">
+                <div class="content">
+                    <div class="header">
+                        <a href="${repoUrl}" target="_blank">${repoName}</a>
+                    </div>
+                    <div class="description">
+                        ${repoDescription}
+                    </div>
+                </div>
+            </div>`;
+          projectsContainer.insertAdjacentHTML('beforeend', projectCard);
+        });
+      }
     });
 });
--- example/github-project.js
+++ example/github-project.js
@@ -1,28 +1,31 @@
 // 自 GitHub 抓取專案資料的程式
 document.addEventListener('DOMContentLoaded', function () {
-    const githubUsername = 'KageRyo';
-    const apiUrl = 'https://api.github.com/users/' + githubUsername + '/repos';
-  
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        const projectsContainer = document.querySelector('.projects');
+  const githubUsername = 'KageRyo';
+  const apiUrl = 'https://api.github.com/users/' + githubUsername + '/repos';
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const projectsContainer = document.querySelector('.projects');
+
+      if (projectsContainer) {
         data.forEach((repo) => {
           const repoName = repo.name;
           const repoUrl = repo.html_url;
           const repoDescription = repo.description || '無描述';
           const projectCard = `
-                    <div class="ts-card">
-                        <div class="content">
-                            <div class="header">
-                                <a href="${repoUrl}" target="_blank">${repoName}</a>
-                            </div>
-                            <div class="description">
-                                ${repoDescription}
-                            </div>
-                        </div>
-                    </div>`;
+            <div class="ts-card">
+                <div class="content">
+                    <div class="header">
+                        <a href="${repoUrl}" target="_blank">${repoName}</a>
+                    </div>
+                    <div class="description">
+                        ${repoDescription}
+                    </div>
+                </div>
+            </div>`;
           projectsContainer.insertAdjacentHTML('beforeend', projectCard);
         });
-      });
-  });  
+      }
+    });
+});
