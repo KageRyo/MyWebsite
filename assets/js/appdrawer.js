ed48673b3fcbf15ea4827a74241b85d199ee596d
KageRyo <k6.developer@gmail.com>
2023-05-02 23:02:32 +0800

    Add App Drawer.

---
+++ assets/js/appdrawer.js
@@ -0,0 +1,23 @@
+// 初始化功能
+document.addEventListener("DOMContentLoaded", () => {
+    initAppDrawer();
+});
+
+// 處理邊緣抽屜的開關
+function initAppDrawer() {
+    const appDrawer = document.querySelector(".ts-app-drawer");
+    const appDrawerOpenButton = document.getElementById("app-drawer-open-button");
+    const appDrawerCloseButton = document.getElementById("app-drawer-close-button");
+
+    appDrawerOpenButton.addEventListener("click", event => {
+        event.preventDefault();
+        appDrawer.classList.remove("is-hidden");
+        appDrawer.classList.add("is-visible");
+    });
+
+    appDrawerCloseButton.addEventListener("click", event => {
+        event.preventDefault();
+        appDrawer.classList.remove("is-visible");
+        appDrawer.classList.add("is-hidden");
+    });
+}
