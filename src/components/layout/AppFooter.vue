ff5d02d44f0796fc1e8827ba9b6f01651cc6c323
Chien-Hsun Chang <kageryo@coderyo.com>
2026-01-23 16:04:51 +0800

    fix: update copyright year dynamically in AppFooter

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="has-flex-center has-top-spaced-small">
     <div class="ts-meta is-secondary is-small">
-      <a href="https://kageryo.coderyo.com/" class="item">&copy; 2023-2025 KageRyo Developer</a>
+      <a href="https://kageryo.coderyo.com/" class="item">&copy; 2023-{{ new Date().getFullYear() }} KageRyo Developer</a>
       <a href="https://coderyo.com/" class="item">CodeRyo 碼凌工作室</a>
   <a href="https://github.com/KageRyo/" class="item has-bottom-spaced">Chien-Hsun Chang 張健勳</a>
     </div>
@@ -9,5 +9,5 @@
 </template>
 
 <script setup>
-// 底部組件不需要特殊邏輯
+const currentYear = new Date().getFullYear();
 </script>
